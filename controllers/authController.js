df492bdaf62899ecc8085c405a5cc58d5843e37b
Nicolas Juarez <juareznico61@gmail.com>
2025-09-04 17:02:47 -0300

    back v2

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,42 +34,47 @@ const authController = {
     },
     // Inicia sesión de un usuario y genera un token JWT
     login: async (req, res) => {
-        try {
-            const { nombre, password } = req.body;
-            const user = await User.findOne({
-                nombre
-            });
-            if (!user) {
-                return res.status(400).json({
-                    message: 'Credenciales inválidas.'
-                });
-            }
-            const isMatch = await bcrypt.compare(password, user.password);
-            if (!isMatch) {
-                return res.status(400).json({
-                    message: 'Credenciales inválidas.'
-                });
-            }
-            if (!user.rol) {
-                user.rol = 'admin';
-                await user.save();
-            }
-            const token = jwt.sign({
-                id: user._id,
-                rol: user.rol
-            }, process.env.JWT_SECRET, {
-                expiresIn: '1h'
-            });
-            res.status(200).json({
-                message: 'Login exitoso.',
-                token
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: 'Error al iniciar sesión.',
-                error
-            });
-        }
+        try {
+            const { nombre, password } = req.body;
+            const user = await User.findOne({
+                nombre
+            });
+            if (!user) {
+                return res.status(400).json({
+                    message: 'Credenciales inválidas.'
+                });
+            }
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({
+                    message: 'Credenciales inválidas.'
+                });
+            }
+            if (!user.rol) {
+                user.rol = 'admin';
+                await user.save();
+            }
+            const token = jwt.sign({
+                id: user._id,
+                rol: user.rol
+            }, process.env.JWT_SECRET, {
+                expiresIn: '1h'
+            });
+            res.status(200).json({
+                message: 'Login exitoso.',
+                token,
+                user: {
+                    id: user._id,
+                    nombre: user.nombre,
+                    rol: user.rol
+                }
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: 'Error al iniciar sesión.',
+                error
+            });
+        }
     }
 };
 
